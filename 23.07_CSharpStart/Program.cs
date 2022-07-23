125130c2af84e0dc2f1dc0f81c9e31a835b71d64
d3zon1x <shostakdima26@gmail.com>
2022-07-23 20:40:07 +0300

    c# start

---
+++ 23.07_CSharpStart/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Text;
+
+namespace CSharp 
+{
+    class Program 
+    {
+        public static void Main(string[] args) 
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            double a, b;
+            Console.WriteLine("Enter a : ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b : ");
+            b = double.Parse(Console.ReadLine());
+            double c = a + b;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("a + b = " + c);
+            }
+        }
+    }
+}
+
